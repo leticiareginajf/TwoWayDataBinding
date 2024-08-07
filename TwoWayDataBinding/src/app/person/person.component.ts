c7a6d644b904558eda9c219392ea30889e70204d
leticiareginajf <leticia.reginajf@gmail.com>
2024-08-07 10:03:18 -0300

    person com ngFor

--- TwoWayDataBinding/src/app/person/person.component.ts
+++ TwoWayDataBinding/src/app/person/person.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-person',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './person.component.css'
 })
 export class PersonComponent {
+
+  @Input({ required: true}) personName: string = '';
+  @Input({ required: true}) personAge: number = 0;
+  @Input({ required: true}) personIndex: number = 0;
+  @Input({ required: true}) isFirst: boolean = false;
+  @Input({ required: true}) isLast: boolean = false;
+  @Input({ required: true}) isOdd: boolean = false;
+  @Input({ required: true}) isSelected: boolean = false;
+
+  @Output('personSelected') onPersonSelectEmitt = new EventEmitter<number>();
+
+
+
+
+  selectPerson(){
+    this.onPersonSelectEmitt.emit(this.personIndex);
+
+  }
 
 }
