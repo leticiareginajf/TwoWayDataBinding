324ad243aba31d47119d1824b3d5d9c7fa2894ca
leticiareginajf <leticia.reginajf@gmail.com>
2024-10-09 09:44:25 -0300

    atuatualizacao

--- TwoWayDataBinding/src/app/app.component.ts
+++ TwoWayDataBinding/src/app/app.component.ts
@@ -7,27 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  fontSize: number = 15;
-  textColor: 'white' | 'orange' = 'white';
-  buttonText: 'Branco' | 'Laranja' = 'Laranja';
-
-  increaseFontSize(){
-
-    this.fontSize += 5;
-
-  }
-
-  toggleFontColor(){
-
-    if(this.textColor === 'white'){
-      this.textColor = 'orange';
-      this.buttonText = 'Branco';
-    } else{
-      this.textColor = 'white';
-      this.buttonText = 'Laranja';
-    }
-
-  }
+  
 
 }
 
@@ -87,5 +67,29 @@ minhaProp = true;
     
     this.personSelectedIndex = index;
   }
+
+
+fontSize: number = 15;
+  textColor: 'white' | 'orange' = 'white';
+  buttonText: 'Branco' | 'Laranja' = 'Laranja';
+
+  increaseFontSize(){
+
+    this.fontSize += 5;
+
+  }
+
+  toggleFontColor(){
+
+    if(this.textColor === 'white'){
+      this.textColor = 'orange';
+      this.buttonText = 'Branco';
+    } else{
+      this.textColor = 'white';
+      this.buttonText = 'Laranja';
+    }
+
+  }
+
 
 */
